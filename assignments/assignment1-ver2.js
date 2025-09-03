02e8d3845e70b35bf98ff7a688c580f498e2f964
lofquista <loal23gk@student.ju.se>
2025-09-03 21:10:26 +0200

    small change of random

--- assignments/assignment1-ver2.js
+++ assignments/assignment1-ver2.js
@@ -22,7 +22,7 @@ translate(-width / 2, -height / 2);
         strokeWeight(18 * Math.random());
 
         //The next two lines are retreived from ChatGPT https://chatgpt.com/share/68b8728a-8948-8002-953f-2e164ed9e63d and https://chatgpt.com/share/68b88335-49e8-8002-9218-8d4ef47d2c53
-    let y = height / 2 + (i - totalArcs/2) * gap; 
+    let y = height / 2 + (i - totalArcs/2) * gap + random(-10, 10); 
    let arcWidth = 450 - Math.abs(i - totalArcs/2) / (totalArcs/2) * 350 + random(-20, 50);
     arc(width/2, y, arcWidth, 50, 5, PI + HALF_PI);
   }
@@ -37,7 +37,7 @@ translate(-width / 2, -height / 2);
        strokeWeight(10 * Math.random());
 
         //The next two lines are retreived from ChatGPT https://chatgpt.com/share/68b8728a-8948-8002-953f-2e164ed9e63d and https://chatgpt.com/share/68b88335-49e8-8002-9218-8d4ef47d2c53
-    let y = height / 2 + (i - totalArcs/2) * gap; 
+    let y = height / 2 + (i - totalArcs/2) * gap + random(-10, 10); 
    let arcWidth = 450 - Math.abs(i - totalArcs/2) / (totalArcs/2) * 350 + random(-50, 20);
     arc(width/2, y, arcWidth, 50, 6, PI + HALF_PI);
   }
