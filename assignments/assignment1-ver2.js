8bd03562467d6bfb4d4c515bdcd6a614799a710e
lofquista <loal23gk@student.ju.se>
2025-09-03 21:04:50 +0200

    Angle and colors assign. 1 ver2

--- assignments/assignment1-ver2.js
+++ assignments/assignment1-ver2.js
@@ -1,22 +1,47 @@
 function setup() {
     createCanvas(innerWidth, innerHeight);
-    background(255);
+    background(255,235,220);
 }
 
-let x = width / 2;
-let y = height / 4;
+const totalArcs = 25;
+const gap = 15;
+
 
 function draw() {
     stroke(0);
     strokeWeight(8);
     noFill();
 
+push();
+translate(width / 2, height / 2);
+rotate(10);
+translate(-width / 2, -height / 2);
 
-   for (let i = 0; i < 20; i++) {
-        stroke(255 * Math.random(), 0, 0);
-        strokeWeight(20 * Math.random());
-    let y = height / 4 + i * 20; 
-    arc(x, y, 350 * Math.random(), 30, 0, PI + HALF_PI);
+   for (let i = 0; i < totalArcs; i++) {
+        stroke(255, 140 * Math.random(), 0);
+        strokeWeight(18 * Math.random());
+
+        //The next two lines are retreived from ChatGPT https://chatgpt.com/share/68b8728a-8948-8002-953f-2e164ed9e63d and https://chatgpt.com/share/68b88335-49e8-8002-9218-8d4ef47d2c53
+    let y = height / 2 + (i - totalArcs/2) * gap; 
+   let arcWidth = 450 - Math.abs(i - totalArcs/2) / (totalArcs/2) * 350 + random(-20, 50);
+    arc(width/2, y, arcWidth, 50, 5, PI + HALF_PI);
   }
+  pop();
+  push();
+translate(width / 2, height / 2);
+rotate(10);
+translate(-width / 2, -height / 2);
+
+   for (let i = 0; i < totalArcs; i++) {
+        stroke(135 * Math.random(), 175 , 255);
+       strokeWeight(10 * Math.random());
+
+        //The next two lines are retreived from ChatGPT https://chatgpt.com/share/68b8728a-8948-8002-953f-2e164ed9e63d and https://chatgpt.com/share/68b88335-49e8-8002-9218-8d4ef47d2c53
+    let y = height / 2 + (i - totalArcs/2) * gap; 
+   let arcWidth = 450 - Math.abs(i - totalArcs/2) / (totalArcs/2) * 350 + random(-50, 20);
+    arc(width/2, y, arcWidth, 50, 6, PI + HALF_PI);
+  }
+  pop();
+  
       noLoop();
     }
