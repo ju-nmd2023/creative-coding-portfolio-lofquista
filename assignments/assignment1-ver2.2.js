b0bdb23fbb0f0fa1c0927066d1a252e8cb8f0867
lofquista <loal23gk@student.ju.se>
2025-09-03 21:21:38 +0200

    small test changes

--- assignments/assignment1-ver2.2.js
+++ assignments/assignment1-ver2.2.js
@@ -3,13 +3,12 @@ function setup() {
     background(255,235,220);
 }
 
-const totalArcs = 25;
-const gap = 15;
+const totalArcs = 12;
+const gap = 25;
 
 
 function draw() {
-    stroke(0);
-    strokeWeight(8);
+
     noFill();
 
 push();
@@ -19,11 +18,11 @@ translate(-width / 2, -height / 2);
 
    for (let i = 0; i < totalArcs; i++) {
         stroke(255, 140 * Math.random(), 0);
-        strokeWeight(18 * Math.random());
+        strokeWeight(30 * Math.random());
 
         //The next two lines are retreived from ChatGPT https://chatgpt.com/share/68b8728a-8948-8002-953f-2e164ed9e63d and https://chatgpt.com/share/68b88335-49e8-8002-9218-8d4ef47d2c53
-    let y = height / 2 + (i - totalArcs/2) * gap + random(-10, 10); 
-   let arcWidth = 450 - Math.abs(i - totalArcs/2) / (totalArcs/2) * 350 + random(-20, 50);
+    let y = height / 2 + (i - totalArcs/2) * gap + random(-100, 50); 
+   let arcWidth = 550 - Math.abs(i - totalArcs/2) / (totalArcs/2) * 650 + random(-20, 50);
     arc(width/2, y, arcWidth, 50, 5, PI + HALF_PI);
   }
   pop();
@@ -34,10 +33,10 @@ translate(-width / 2, -height / 2);
 
    for (let i = 0; i < totalArcs; i++) {
         stroke(135 * Math.random(), 175 , 255);
-       strokeWeight(10 * Math.random());
+       strokeWeight(5 * Math.random());
 
         //The next two lines are retreived from ChatGPT https://chatgpt.com/share/68b8728a-8948-8002-953f-2e164ed9e63d and https://chatgpt.com/share/68b88335-49e8-8002-9218-8d4ef47d2c53
-    let y = height / 2 + (i - totalArcs/2) * gap + random(-10, 10); 
+    let y = height / 2 + (i - totalArcs/2) * gap + random(-50, 100); 
    let arcWidth = 450 - Math.abs(i - totalArcs/2) / (totalArcs/2) * 350 + random(-50, 20);
     arc(width/2, y, arcWidth, 50, 6, PI + HALF_PI);
   }
