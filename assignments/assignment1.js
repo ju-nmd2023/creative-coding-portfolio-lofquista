e4426605d3f87e6639038123d03cd1045851b139
lofquista <loal23gk@student.ju.se>
2025-09-03 16:11:33 +0200

    Changed stroke weight

--- assignments/assignment1.js
+++ assignments/assignment1.js
@@ -24,12 +24,12 @@ for (let i=0; i < Yamount; i++) {
     translate(x, y);
 
   stroke(0);
-      strokeWeight(0.5);
+      strokeWeight(1);
       
       // rect(0, 0, size, size);
 
       beginShape();
-      for (let s = 0; s < 15; s++) {
+      for (let s = 0; s < 9; s++) {
         vertex(random(0, size), random(0, size));
       }
       endShape();
