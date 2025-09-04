059a8b41187bdeb723c2cba3dae7f46f84d39a67
lofquista <loal23gk@student.ju.se>
2025-09-04 11:15:39 +0200

    Changed description

--- assignments/assignment1-ver2.2.js
+++ assignments/assignment1-ver2.2.js
@@ -1,6 +1,6 @@
 function setup() {
     createCanvas(innerWidth, innerHeight);
-    background(255,235,220);
+    background(0);
 }
 
 const totalArcs = 12;
@@ -17,7 +17,7 @@ rotate(10);
 translate(-width / 2, -height / 2);
 
    for (let i = 0; i < totalArcs; i++) {
-        stroke(255, 140 * Math.random(), 0);
+        stroke(25, 255 * Math.random(), 0);
         strokeWeight(30 * Math.random());
 
         //The next two lines are retreived from ChatGPT https://chatgpt.com/share/68b8728a-8948-8002-953f-2e164ed9e63d and https://chatgpt.com/share/68b88335-49e8-8002-9218-8d4ef47d2c53
@@ -32,7 +32,7 @@ rotate(10);
 translate(-width / 2, -height / 2);
 
    for (let i = 0; i < totalArcs; i++) {
-        stroke(135 * Math.random(), 175 , 255);
+        stroke(255 * Math.random(), 255 , 255);
        strokeWeight(5 * Math.random());
 
         //The next two lines are retreived from ChatGPT https://chatgpt.com/share/68b8728a-8948-8002-953f-2e164ed9e63d and https://chatgpt.com/share/68b88335-49e8-8002-9218-8d4ef47d2c53
