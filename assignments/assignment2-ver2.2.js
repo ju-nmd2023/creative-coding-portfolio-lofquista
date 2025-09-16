7eb6eb5b5ca963a3cd22388a948888b0e5eafd0a
lofquista <loal23gk@student.ju.se>
2025-09-16 19:45:31 +0200

    Test on ver2.2

--- assignments/assignment2-ver2.2.js
+++ assignments/assignment2-ver2.2.js
@@ -7,15 +7,14 @@ function setup() {
     position = createVector(200, 200);
         velocity = createVector(5, 8);
         background(0);
-frameRate(20);
+frameRate(40);
 }
 
 function draw() {
-
     push();
     stroke(200, 100, random(255));
-    strokeWeight(1);
-line(position.x, position.y, 80, 20, 0);
+    strokeWeight(20);
+line(position.x, position.y, 50, innerHeight / 2);
     pop();
 
 
@@ -23,11 +22,9 @@ line(position.x, position.y, 80, 20, 0);
 const mouse = createVector(mouseX, mouseY);
 acceleration = p5.Vector.sub(mouse, position);
 acceleration.normalize();
-acceleration.mult(0.5);
+acceleration.mult(0.8);
 
 velocity.add(acceleration);
 velocity.limit(10);
     position.add(velocity);
-
-
 }
