8ab31f6dc112c1e270ba91c12fadb440febfd6a5
lofquista <loal23gk@student.ju.se>
2025-09-19 13:28:13 +0200

    Testing ver3

--- assignments/assignment2-ver2.2.js
+++ assignments/assignment2-ver2.2.js
@@ -1,20 +1,26 @@
 let position;
 let velocity;
 let acceleration;
+let colors = [
+    [137, 207, 240],
+    [255, 182, 193],
+    [25, 25, 62]
+];
 
 function setup() {
     createCanvas(innerWidth, innerHeight);
-    position = createVector(200, 200);
+    position = createVector(width / 2, height / 2);
         velocity = createVector(5, 8);
         background(0);
 frameRate(40);
+
 }
 
 function draw() {
     push();
-    stroke(200, 100, random(255));
+    stroke(random(colors));
     strokeWeight(20);
-line(position.x, position.y, 50, innerHeight / 2);
+line(width / 2, height / 2, position.x, position.y);
     pop();
 
 
@@ -27,4 +33,10 @@ acceleration.mult(0.8);
 velocity.add(acceleration);
 velocity.limit(10);
     position.add(velocity);
+}
+
+function mouseClicked() {
+    push();
+    strokeWeight(random(50));
+    pop();
 }
