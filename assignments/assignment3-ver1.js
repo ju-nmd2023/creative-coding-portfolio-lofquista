cb329fe49a85c33168f6e42ce38e523ffdec7d25
lofquista <loal23gk@student.ju.se>
2025-09-23 19:37:36 +0200

    Added ass3 to json

---
+++ assignments/assignment3-ver1.js
@@ -0,0 +1,53 @@
+// Plan: When clicking on buttons on dj table, the dancing humans from assignment 1 week 3 are moving (daning)
+
+const Xsize = 20;
+const Ysize = 80;
+const gap = 15;
+const Yamount = 4;
+const Xamount = 18;
+const ellipseSize = 20;
+
+
+function setup() {
+    createCanvas(innerWidth, innerHeight);
+    frameRate(3);
+}
+
+function draw() {
+  background(255);
+
+push();
+rect(width / 2, 0, 100, 100);
+pop();
+
+  noStroke();
+  noFill();
+
+let y = (height - Ysize * Yamount - gap * (Yamount - 1)) / 2;
+
+
+for (let i=0; i < Yamount; i++) {
+  let x = (width - Xsize * Xamount - gap * (Xamount - 1)) / 2;
+
+  for (let k=0; k < Xamount; k++) {
+    push();
+    translate(x, y);
+
+  stroke(74, 125, 65);
+  strokeWeight(1);
+
+
+      beginShape();
+      for (let s = 0; s < 12; s++) {
+        vertex(random(0, Xsize), random(0, Ysize));
+        ellipse(10, 0, ellipseSize);
+      }
+      endShape();
+      
+  pop(); 
+
+  x += Xsize + gap;
+}
+y += Ysize + gap;
+}
+}
