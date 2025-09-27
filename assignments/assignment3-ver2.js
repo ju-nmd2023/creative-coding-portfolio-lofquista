401dae7f96134c2bf201655a641f9b21c068a2cc
lofquista <loal23gk@student.ju.se>
2025-09-27 18:18:48 +0200

    Initial commit ass3 ver2

---
+++ assignments/assignment3-ver2.js
@@ -0,0 +1,46 @@
+// Plan: when the mouse reaches a certain area of the canvas it plays a certain note
+
+let startPoint;
+let position;
+let velocity;
+let acceleration;
+let startLines = false;
+
+function setup() {
+    createCanvas(innerWidth, innerHeight);
+        background(0);
+frameRate(30);
+}
+
+
+function drawLines() {
+       push();
+    stroke(255, 255, 255);
+    strokeWeight(1);
+line(startPoint.x, startPoint.y, position.x, position.y);
+pop();
+}
+
+
+function draw() {
+if (startLines) {
+    drawLines();
+    
+    const mouse = createVector(mouseX, mouseY);
+    acceleration = p5.Vector.sub(mouse, position);
+    acceleration.normalize();
+    acceleration.mult(0.5);
+    
+    velocity.add(acceleration);
+    velocity.limit(10);
+        position.add(velocity);
+}
+}
+
+function mouseClicked() {
+    startLines = !startLines;
+
+    startPoint = createVector(mouseX, mouseY);
+    position = createVector(mouseX, mouseY);
+    velocity = createVector(5, 8);
+}
